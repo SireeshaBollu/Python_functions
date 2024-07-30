9a4042e5803a776c3785519456d19de7c689a4b2
SireeshaBollu <sireeshabollu110125@gmail.com>
2024-07-30 08:17:40 -0500

    message

---
+++ check_prime_numbers.py
@@ -0,0 +1,29 @@
+"""Create an array that has user defined inputs and with the help of for loop,
+fetch all the prime numbers and print the numbers.
+"""
+
+def check_prime_number(input_num):
+	for i in range(2, input_num):
+		if input_num % i == 0:
+			return False
+	return True
+
+my_array = []
+a = int(input("Please input a number or Enter 0 to exit...\n"))
+
+
+while a:
+	my_array.append(a)
+	a = int(input("Please input a number or Enter 0 to exit...\n"))
+
+print(my_array)
+result = []
+
+
+for i in my_array:
+	if check_prime_number(i):
+		result.append(i)
+
+print(f"Prime numbers are /n - {result}")
+
+
