290c9eb7f0c8157b9010ab68bc479561c2be655f
SireeshaBollu <sireeshabollu110125@gmail.com>
2024-07-31 07:52:55 -0500

    commit

---
+++ python_dict.py
@@ -0,0 +1,22 @@
+# a = {
+# 	"h": 1,
+# 	"7865": "Sireesha"
+# }
+# user_input = "123456"
+# print(a.get(user_input, "Number not found"))
+# input_str = "hello"
+# result = {}
+# for each_char in input_str:
+# 	if each_char not in result:
+# 		result[each_char] = 1
+# 	else:
+# 		result[each_char]  = result[each_char]  + 1
+# print(result)
+
+# a = {'a': 1, 'b': 2}
+# # result = {1: 'a', 2: 'b'}
+# result = {}
+# for key, value in a.items():
+# 	result[value] = key
+
+# print(result)
